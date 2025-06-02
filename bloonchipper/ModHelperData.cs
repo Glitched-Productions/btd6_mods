88f9dece2a0a766ba3ef68a3394cd6a18b9b0571
EugRogel Bobis <bobiseugrogel@gmail.com>
2025-06-02 08:23:54 -0600

    changed subpath to working one

--- bloonchipper/ModHelperData.cs
+++ bloonchipper/ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Name = "bloonchipperfixed";
 
     public const string Description = "adds the bloonchipper to the game";
- public const string SubPath = "bloonchipperfixed";
+ public const string SubPath = "bloonchipper";
  public const string DllName = "bloonchipperfixed.dll";
  public const string WorksOnVersion = "45";
 
